cbfca326f80fbe4361ef8050070c84ef382e2c80
Olga Blud <olga.blud@gmail.com>
2024-08-02 13:54:37 +0300

    add Local Storage contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
+import initialContacts from "./data/data.json";
 
 function App() {
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const contactsFromLSData = JSON.parse(
+      window.localStorage.getItem("contacts")
+    );
+
+    console.log(contactsFromLSData.contacts.length);
+    if (contactsFromLSData.contacts.length > 4) {
+      return contactsFromLSData.contacts;
+    }
+    return initialContacts;
+  });
 
   const [searchValue, setSearchValue] = useState("");
 
@@ -27,6 +33,9 @@ function App() {
       prevContactList.filter((contact) => contact.id !== contactId)
     );
   };
+  useEffect(() => {
+    window.localStorage.setItem("contacts", JSON.stringify({ contacts }));
+  }, [contacts]);
   console.log(contacts);
   return (
     <div>
